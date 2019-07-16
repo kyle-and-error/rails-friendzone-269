c664f6ca5ab0dc43873646900ea958d4eaec721f
Kyle Cho <kylescho99@gmail.com>
2019-07-16 09:47:53 +0200

    uncommmented validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,6 @@ class Event < ApplicationRecord
   has_many :potential_attendees
   has_many :users, through: :potential_attendees
 
-  # validates :user, :name, :photo, :people_needed, :location, :start_time, :end_time, presence: true
-  # validates_numericality_of :people_needed, greater_than: 0
+  validates :user, :name, :photo, :people_needed, :location, :start_time, :end_time, presence: true
+  validates_numericality_of :people_needed, greater_than: 0
 end
