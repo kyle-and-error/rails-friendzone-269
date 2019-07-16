2c6c09bc369ed67292fe76840179b341df77e77d
Kyle Cho <kylescho99@gmail.com>
2019-07-16 10:40:29 +0200

    updated associatons between event and user

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
-  belongs_to :user
+  belongs_to :host, class_name: "User"
   has_many :potential_attendees
   has_many :users, through: :potential_attendees
 
-  validates :user, :name, :photo, :people_needed, :location, :start_time, :end_time, presence: true
+  validates :host, :name, :photo, :people_needed, :location, :start_time, :end_time, presence: true
   validates_numericality_of :people_needed, greater_than: 0
 end
