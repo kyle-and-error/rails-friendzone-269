1f8c9dea6cc4db923d549176f0bec12ab1120872
Kyle Cho <kylescho99@gmail.com>
2019-07-16 11:11:01 +0200

    Final validations and destroy dependencies

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :host, class_name: "User"
-  has_many :potential_attendees
+  has_many :potential_attendees, dependent: :destroy
   has_many :users, through: :potential_attendees
 
   validates :host, :name, :photo, :people_needed, :location, :start_time, :end_time, presence: true
