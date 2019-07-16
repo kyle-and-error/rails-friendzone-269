3903560176660d2ed7c49154c46e412224b75043
Kyle Cho <kylescho99@gmail.com>
2019-07-16 09:46:55 +0200

    Added associations for events using through potential_attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :events
+  has_many :potential_attendees
+  has_many :users, through: :potential_attendees
 
-  validates :user, :name, :photo, :people_needed, :location, :start_time, :end_time, presence: true
-  validates_numericality_of :people_needed, greater_than: 0
+  # validates :user, :name, :photo, :people_needed, :location, :start_time, :end_time, presence: true
+  # validates_numericality_of :people_needed, greater_than: 0
 end
