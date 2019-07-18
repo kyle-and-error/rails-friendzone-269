89185291b4ee408698e3d31c98aa25218794fd0c
Kyle Cho <kylescho99@gmail.com>
2019-07-18 15:02:14 +0200

    Geocoder and updating seeds

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,11 @@ class Event < ApplicationRecord
   belongs_to :host, class_name: "User"
   has_many :potential_attendees, dependent: :destroy
   has_many :users, through: :potential_attendees
+  geocoded_by :address
 
-  validates :host, :name, :people_needed, :location, :start_time, :end_time, presence: true
+  validates :host, :name, :people_needed, :address, :start_time, :end_time, presence: true
   validates_numericality_of :people_needed, greater_than: 0
+  after_validation :geocode, if: :will_save_change_to_address?
 
   def attendees
     potential_attendees.select { |potential_attendee| potential_attendee.status == "accepted" }
