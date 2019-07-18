76c391b8913c60518c5bca7339db896f8eadb1b9
Paulina Gernandt <paulina@gernandtnet.de>
2019-07-18 16:04:36 +0200

    alotofchange

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events, only: [:index, :show, :new, :create] do
     resources :potential_attendees, only: [:index, :new, :create, :destory]
-    edit "potential_attendees/:id/accept" as: :accept_request
-    edit "potential_attendees/:id/reject" as: :reject_request
+    #edit "potential_attendees/:id/accept" as: :accept_request
+    #edit "potential_attendees/:id/reject" as: :reject_request
 
   end
   resources :potential_attendees, only: :show
